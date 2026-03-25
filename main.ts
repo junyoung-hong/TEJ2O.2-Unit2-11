66e2d91191d10c07b73775fac937e4bc9a5f17cd
junyoung-hong <junyoung.hong@stu.ocsb.ca>
2026-03-25 12:42:27 -0400

    Number code

--- main.ts
+++ main.ts
@@ -1,8 +1,44 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2026 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Junyoung
+ * Created on: Mar 2026
+ * This program compare two random numbers.
 */
 
-basic.showString('Hello, World!')
+//variables
+let number1 : number
+let number2 : number 
+
+//start
+basic.showIcon(IconNames.Happy)
+number1 = randint(0, 99)
+number2 = randint(0, 99)
+
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    basic.showString( number1 .toString())
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+   
+input.onButtonPressed(Button.B, function() {
+    basic.clearScreen()
+    basic.showString( number2 .toString())
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onGesture(Gesture.Shake, function() {
+    basic.clearScreen()
+
+    if (number1 < number2) { 
+        basic.clearScreen()
+        basic.showString(number1.toString() + '<' + number2.toString())
+        basic.showIcon(IconNames.Happy)
+    
+    } else { 
+        basic.clearScreen()
+        basic.showString(number1.toString() + '>' + number2.toString())
+        basic.showIcon(IconNames.Sad)
+    }
+})
