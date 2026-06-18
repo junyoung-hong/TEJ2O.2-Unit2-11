56d5a4aba2ae6372438095df9a9efb180b67da6b
junyoung-hong <junyoung.hong@stu.ocsb.ca>
2026-06-18 17:50:22 -0400

    code

--- main.ts
+++ main.ts
@@ -4,39 +4,44 @@
  * This program compare two random numbers.
 */
 
-//variables
+// variables
 basic.showIcon(IconNames.Happy)
 const number1 = randint(0, 99)
 const number2 = randint(0, 99)
 
-//Pressed A
-input.onButtonPressed(Button.A, function() {
+// Pressed A
+input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
-    basic.showString( number1 .toString())
-    basic.clearScreen()
-    basic.showIcon(IconNames.Happy)
-})
-   
-//Pressed B
-input.onButtonPressed(Button.B, function() {
-    basic.clearScreen()
-    basic.showString( number2 .toString())
+    basic.showString(convertToText(number1))
     basic.clearScreen()
     basic.showIcon(IconNames.Happy)
 })
 
-//Shake the microbit
-input.onGesture(Gesture.Shake, function() {
+// Pressed B
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showString(convertToText(number2))
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+// Shake the microbit
+input.onGesture(Gesture.Shake, function () {
     basic.clearScreen()
 
-    if (number1 < number2) { 
+    if (number1 < number2) {
         basic.clearScreen()
-        basic.showString(number1.toString() + '<' + number2.toString())
+        basic.showString(convertToText(number1) + '<' + convertToText(number2))
         basic.showIcon(IconNames.Happy)
-    
+
+    } else if (number1 > number2) { 
+        basic.clearScreen()
+        basic.showString(convertToText(number1) + '>' + convertToText(number2))
+        basic.showIcon(IconNames.Sad)
+
     } else { 
         basic.clearScreen()
-        basic.showString(number1.toString() + '>' + number2.toString())
-        basic.showIcon(IconNames.Sad)
+        basic.showString(convertToText(number1) + '=' + convertToText(number2))
+        basic.showIcon(IconNames.Surprised)
     }
 })
